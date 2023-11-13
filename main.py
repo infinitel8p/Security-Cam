423a71793f9a7e2d99b57235b53164fa36ae7a9f
Ludo <ludovico.ferrara@ruhr-uni-bochum.de>
2023-11-13 13:55:41 +0100

    Add WebSocket streaming functionality to main.py

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
+import io
 import json
 import logging
 import os
 import time
 from datetime import datetime
-import picamera
+
 import RPi.GPIO as GPIO
+import picamera
 from dotenv import load_dotenv
+import websocket
+import threading
+
 from functions import *
+
 
 # Set up logging with custom timestamp format
 logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s - %(levelname)s - %(message)s',
+                    format='%(asctime)s - %(levelname)s - [%(threadName)s] - %(message)s',
                     datefmt='%d-%m-%Y %H:%M:%S')
 
 logging.info("Initializing...")
@@ -38,7 +44,12 @@ if isinstance(TARGET_AP_MAC_ADDRESSES, str):
 
 # Initialize the camera
 camera = picamera.PiCamera()
+camera.annotate_background = picamera.Color('black')
+jpeg_quality = 85
+stream = io.BytesIO()
 recording = False
+streaming = True
+should_send_frames = False
 
 # Set up GPIO
 GPIO.setmode(GPIO.BCM)
@@ -46,10 +57,70 @@ GPIO.setwarnings(False)
 Digital_Pin = 22
 GPIO.setup(Digital_Pin, GPIO.IN)
 
-# Enable timestamp on the video
-camera.annotate_background = picamera.Color('black')
+
+def send_frames(ws):
+    global should_send_frames, streaming
+
+    while streaming:
+        if not should_send_frames:
+            time.sleep(0.1)
+            continue
+
+        camera.capture(stream, format='jpeg',
+                       use_video_port=True, quality=jpeg_quality)
+        if should_send_frames:
+            stream.seek(0)
+            buffer = stream.read()
+            ws.send(buffer, opcode=websocket.ABNF.OPCODE_BINARY)
+
+        stream.seek(0)
+        stream.truncate()
+
+    camera.close()
+    logging.info("Camera released and thread terminating...")
+
+
+def on_open(ws):
+    def run(*args):
+        logging.info("WebSocket opened and thread started")
+        global streaming
+        ws.send("Python Script")
+        send_frames(ws)
+        streaming = False
+
+    threading.Thread(target=run, name="WebSocketStreamThread").start()
+
+
+def on_close(ws, close_status_code, close_msg):
+    global streaming
+    logging.info("WebSocket closed")
+    streaming = False
+
+
+def on_message(ws, message):
+    global should_send_frames
+    if message == "START":
+        logging.info("Streaming requested, sending frames...")
+        should_send_frames = True
+    elif message == "STOP":
+        logging.info("Streaming stopped, no longer sending frames...")
+        should_send_frames = False
+
+
+def start_websocket():
+    ws = websocket.WebSocketApp(
+        "ws://localhost:3000",
+        on_open=on_open,
+        on_message=on_message,
+        on_close=on_close
+    )
+    ws.run_forever()
+
 
 try:
+    threading.Thread(target=start_websocket,
+                     name="WebSocketSetupThread", daemon=True).start()
+
     while True:
         update_annotation(camera)
         if GPIO.input(Digital_Pin):
@@ -75,9 +146,11 @@ try:
                     recording = True
         time.sleep(1)
 except KeyboardInterrupt:
+    streaming = False
     if recording:
         camera.stop_recording()
     camera.close()
-    logging.info("Script manually interrupted! Recording stopped.")
+    logging.info(
+        "Script interrupted by user (Ctrl+C! Recording stopped. Exiting...")
 finally:
     GPIO.cleanup()
