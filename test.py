7e3fdac39e1c7023acf7c0a1840fc950a9910305
Ludo <ludovico.ferrara@ruhr-uni-bochum.de>
2023-11-13 13:42:48 +0100

    Add Bluetooth and AP detection for recording, improve logging, and refactor code

--- test.py
+++ test.py
@@ -1,19 +1,65 @@
-from picamera import PiCamera
 import io
+import json
+import logging
+import os
+import time
+from datetime import datetime
+
+import RPi.GPIO as GPIO
+import picamera
+from dotenv import load_dotenv
 import websocket
 import threading
-import time
-import datetime
 
+from functions import *
+
+
+# Set up logging with custom timestamp format
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s',
+                    datefmt='%d-%m-%Y %H:%M:%S')
+
+logging.info("Initializing...")
+
+# Try to load config from .env file
+logging.info("Loading config from .env file...")
+load_dotenv()
+
+TARGET_BT_ADDRESSES = os.getenv("TARGET_BT_ADDRESSES")
+TARGET_AP_MAC_ADDRESSES = os.getenv("TARGET_AP_MAC_ADDRESSES")
+
+# If not found in .env, fall back to config.json
+if not TARGET_BT_ADDRESSES or not TARGET_AP_MAC_ADDRESSES:
+    logging.info(".env not found, falling back to config.json file...")
+    with open('config.json', 'r') as f:
+        config = json.load(f)
+        TARGET_BT_ADDRESSES = config["TARGET_BT_ADDRESSES"]
+        TARGET_AP_MAC_ADDRESSES = config["TARGET_AP_MAC_ADDRESSES"]
+
+# Convert the comma-separated string from .env to a list
+if isinstance(TARGET_BT_ADDRESSES, str):
+    TARGET_BT_ADDRESSES = TARGET_BT_ADDRESSES.split(',')
+if isinstance(TARGET_AP_MAC_ADDRESSES, str):
+    TARGET_AP_MAC_ADDRESSES = TARGET_AP_MAC_ADDRESSES.split(',')
+
+# Initialize the camera
+camera = picamera.PiCamera()
+camera.annotate_background = picamera.Color('black')
+jpeg_quality = 85
+stream = io.BytesIO()
+recording = False
 streaming = True
 should_send_frames = False
+
+# Set up GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+Digital_Pin = 22
+GPIO.setup(Digital_Pin, GPIO.IN)
 
 
 def send_frames(ws):
     global should_send_frames, streaming
-    camera = PiCamera()
-    jpeg_quality = 85
-    stream = io.BytesIO()
 
     while streaming:
         if not should_send_frames:
@@ -63,15 +109,49 @@ def on_message(ws, message):
         should_send_frames = False
 
 
-if __name__ == "__main__":
-    try:
-        ws = websocket.WebSocketApp(
-            "ws://localhost:3000",
-            on_open=on_open,
-            on_message=on_message,
-            on_close=on_close
-        )
-        ws.run_forever()
-    except KeyboardInterrupt:
-        print("Script interrupted by user (Ctrl+C). Exiting...")
-        streaming = False
+def start_websocket():
+    ws = websocket.WebSocketApp(
+        "ws://localhost:3000",
+        on_open=on_open,
+        on_message=on_message,
+        on_close=on_close
+    )
+    ws.run_forever()
+
+
+try:
+    threading.Thread(target=start_websocket, daemon=True).start()
+
+    while True:
+        update_annotation(camera)
+        if GPIO.input(Digital_Pin):
+            logging.info("Door is closed.")
+            if recording:
+                logging.info("Recording was in progress. Pausing recording.")
+                camera.stop_recording()
+                recording = False
+        else:
+            logging.warning("Door is open.")
+            # Check if any of the smartphones' Bluetooth addresses are visible or if they're connected to the AP
+            if is_device_connected_to_bt(TARGET_BT_ADDRESSES) or is_device_connected_to_ap(TARGET_AP_MAC_ADDRESSES):
+                if recording:
+                    logging.info("Device detected. Stop recording.")
+                    camera.stop_recording()
+                    recording = False
+            # If no device is connected, start recording
+            else:
+                if not recording:
+                    logging.info("Device not detected. Start recording.")
+                    camera.start_recording(
+                        f"./server/public/recordings/video_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.h264")
+                    recording = True
+        time.sleep(1)
+except KeyboardInterrupt:
+    streaming = False
+    if recording:
+        camera.stop_recording()
+    camera.close()
+    logging.info(
+        "Script interrupted by user (Ctrl+C! Recording stopped. Exiting...")
+finally:
+    GPIO.cleanup()
