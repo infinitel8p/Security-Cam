c80a7740094bf94547a3d87ea5331ec4041d533d
InfiniteL8p <bigludovico@aol.com>
2023-11-14 23:40:15 +0100

    updated design

--- server/public/js/script.js
+++ server/public/js/script.js
@@ -2,15 +2,60 @@ document.addEventListener('DOMContentLoaded', () => {
     const navItems = document.querySelectorAll('.nav-item');
 
     navItems.forEach(item => {
-        const navLink = item.querySelector('.nav-link');
         let currentHref = window.location.href;
 
         if (currentHref.endsWith('/')) {
             currentHref += 'index.html';
         }
 
-        if (navLink.href === currentHref) {
-            navLink.classList.add('active');
+        if (item.href === currentHref) {
+            item.classList.add('active');
         }
     });
+});
+
+const side_bar_btns = document.querySelectorAll("#sidebar-btn");
+
+side_bar_btns.forEach((elem) => {
+    elem.addEventListener("click", () => {
+        for (let index = 0; index < side_bar_btns.length; index++) {
+            side_bar_btns[index].classList.remove("active");
+        }
+        elem.classList.add("active");
+    });
+});
+
+let min = true;
+
+document.querySelector(".window__close").addEventListener("click", () => {
+    document.querySelector(".container").style.display = "none";
+
+    setTimeout(() => {
+        window.alert(
+            "Oh No! What did you do? Now you have to refresh to open the app again"
+        );
+    }, 500);
+});
+
+document.querySelector(".window__maximize").addEventListener("click", () => {
+    if (min == false) {
+        min = true;
+        console.log(min);
+        document.querySelector(".container").style.width = "90%";
+        document.querySelector(".container").style.height = "90%";
+    } else {
+        min = false;
+        document.querySelector(".container").style.width = "100%";
+        document.querySelector(".container").style.height = "100%";
+    }
+});
+
+document.querySelector(".window__minimize").addEventListener("click", () => {
+    document.querySelector(".container").style.transform = "scale(0)";
+
+    setTimeout(() => {
+        window.alert(
+            "The app is minimized but cannot be opened again because the virtual macos crashed!"
+        );
+    }, 500);
 });
